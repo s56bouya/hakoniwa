1e2c3a0e38c673278d4bec1ad38443f56d88f8dd
Tetsuya Imamura <s56bouya@gmail.com>
2023-05-25 17:01:53 +0900

    Update

--- inc/init/class-scripts.php
+++ inc/init/class-scripts.php
@@ -12,8 +12,6 @@ class Scripts {
 
 		add_action( 'wp_enqueue_scripts', [ $this, 'read_scripts' ] );
 		
-		add_action( 'admin_enqueue_scripts', [ $this, 'admin_read_scripts' ] );
-
 		add_action( 'after_setup_theme', [ $this, 'add_editor_styles' ], 10 );
 
 	}
@@ -22,6 +20,7 @@ class Scripts {
 	 * Enqueue Front End Scripts & Styles
 	 */
 	public function read_scripts() {
+
 		$theme_version  = wp_get_theme()->get( 'Version' );
 		$version_string = is_string( $theme_version ) ? $theme_version : false;
 
@@ -43,26 +42,11 @@ class Scripts {
 		* Add Style
 		*/ 
 
-		// front
 		$front_end_css = apply_filters( Define::value( 'theme_name' ) . '_enqueue_front_end_css', get_template_directory_uri() . '/assets/css/front-end.css' );
 
 		wp_register_style( Define::value( 'theme_name' ) . '-front-end', $front_end_css, array(), $version_string, 'all' );
 		wp_enqueue_style( Define::value( 'theme_name' ) . '-front-end' );
 
-	}
-
-	/**
-	 * Admin Style & Script
-	 */
-	public function admin_read_scripts() {
-
-		// Add Localize
-		$localize_data = array( 
-			'themeName' => Define::value( 'theme_name' )
-		);
-
-		wp_localize_script( 'common', 'object', $localize_data );
-		
 	}
 
 	/**
