5ca724ce3392a707a5be52f4f6b64b7d72fc738a
Tetsuya Imamura <s56bouya@gmail.com>
2023-06-17 08:31:51 +0900

    Update

--- inc/init/class-settings.php
+++ inc/init/class-settings.php
@@ -38,9 +38,9 @@ class Settings {
 	 */
 	public function default_thumbnail( $html, $post_id, $post_thumbnail_id ){
 
-		if( is_archive() || is_front_page() || is_search() ){
+		if( ! is_singular() ){
 
-			if( ! has_post_thumbnail( $post_id ) && 'page' !== get_option( 'show_on_front' ) ){
+			if( ! has_post_thumbnail( $post_id ) ){
 
 				$html = '<div class="no-image"><svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"></path><line x1="15" y1="8" x2="15.01" y2="8"></line><rect x="4" y="4" width="16" height="16" rx="3"></rect><path d="M4 15l4 -4a3 5 0 0 1 3 0l5 5"></path><path d="M14 14l1 -1a3 5 0 0 1 3 0l2 2"></path></svg></div>';
 
