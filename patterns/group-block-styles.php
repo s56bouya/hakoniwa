46c3f37073db84cd1565bf0abe6f13773e01526e
Tetsuya Imamura <s56bouya@gmail.com>
2023-06-30 13:45:33 +0900

    Update

---
+++ patterns/group-block-styles.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Title: Group Block Styles
+ * Slug: hakoniwa/group-block-styles
+ * Categories: Hakoniwa
+*/
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}}},"className":"is-style-hakoniwa-group-stripe-01","layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-hakoniwa-group-stripe-01" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:paragraph -->
+<p>Stripe</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}}},"backgroundColor":"primary","className":"is-style-hakoniwa-group-stripe-01","layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-hakoniwa-group-stripe-01 has-primary-background-color has-background" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:paragraph -->
+<p>Stripe</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}}},"className":"is-style-hakoniwa-group-dot-01","layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-hakoniwa-group-dot-01" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:paragraph -->
+<p>Dot</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"color":{"background":"#185894"}},"className":"is-style-hakoniwa-group-dot-01","layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-hakoniwa-group-dot-01 has-background" style="background-color:#185894;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:paragraph {"textColor":"base"} -->
+<p class="has-base-color has-text-color">Dot</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}}},"className":"is-style-hakoniwa-group-check-01","layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-hakoniwa-group-check-01" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:paragraph -->
+<p>Check</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"color":{"background":"#96dbee"}},"className":"is-style-hakoniwa-group-check-01","layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-hakoniwa-group-check-01 has-background" style="background-color:#96dbee;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:paragraph -->
+<p>Check</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
---
+++ patterns/image-block-styles.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Title: Image Block Styles
+ * Slug: hakoniwa/image-block-styles
+ * Categories: gallery,Hakoniwa
+*/
+?>
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":759,"sizeSlug":"large","linkDestination":"none","className":"is-style-rounded"} -->
+<figure class="wp-block-image size-large is-style-rounded"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/sample/image02.jpg" alt="" class="wp-image-759"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":759,"sizeSlug":"large","linkDestination":"none","className":"is-style-hakoniwa-image-circle-01"} -->
+<figure class="wp-block-image size-large is-style-hakoniwa-image-circle-01"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/sample/image02.jpg" alt="" class="wp-image-759"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":759,"sizeSlug":"large","linkDestination":"none","className":"is-style-hakoniwa-image-rhombus-01"} -->
+<figure class="wp-block-image size-large is-style-hakoniwa-image-rhombus-01"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/sample/image02.jpg" alt="" class="wp-image-759"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":759,"sizeSlug":"large","linkDestination":"none","className":"is-style-hakoniwa-image-ink-01"} -->
+<figure class="wp-block-image size-large is-style-hakoniwa-image-ink-01"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/sample/image02.jpg" alt="" class="wp-image-759"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":759,"sizeSlug":"large","linkDestination":"none","className":"is-style-hakoniwa-image-ink-02"} -->
+<figure class="wp-block-image size-large is-style-hakoniwa-image-ink-02"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/sample/image02.jpg" alt="" class="wp-image-759"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":759,"sizeSlug":"large","linkDestination":"none","className":"is-style-hakoniwa-image-ink-03"} -->
+<figure class="wp-block-image size-large is-style-hakoniwa-image-ink-03"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/sample/image02.jpg" alt="" class="wp-image-759"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":759,"sizeSlug":"large","linkDestination":"none","className":"is-style-hakoniwa-image-fluid-01"} -->
+<figure class="wp-block-image size-large is-style-hakoniwa-image-fluid-01"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/sample/image02.jpg" alt="" class="wp-image-759"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":759,"sizeSlug":"large","linkDestination":"none","className":"is-style-hakoniwa-image-fluid-02"} -->
+<figure class="wp-block-image size-large is-style-hakoniwa-image-fluid-02"><img src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/sample/image02.jpg" alt="" class="wp-image-759"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":759,"sizeSlug":"large","linkDestination":"none","className":"is-style-hakoniwa-image-fluid-03"} -->
+<figure class="wp-block-image size-large is-style-hakoniwa-image-fluid-03"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/sample/image02.jpg" alt="" class="wp-image-759"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
