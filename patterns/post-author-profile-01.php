bab6cd599e1d2d6732cf1ccc93077bc944965998
Tetsuya Imamura <s56bouya@gmail.com>
2023-07-05 17:32:28 +0900

    Update

---
+++ patterns/post-author-profile-01.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Title: Post Author Profile 01
+ * Slug: hakoniwa/post-author-profile-01
+ * Categories: Hakoniwa
+ */
+?>
+<!-- wp:group {"backgroundColor":"tertiary","layout":{"type":"default"}} -->
+<div class="wp-block-group has-tertiary-background-color has-background"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:avatar {"size":48,"align":"center"} /-->
+
+<!-- wp:post-author-name {"textAlign":"center"} /-->
+
+<!-- wp:social-links {"iconColor":"contrast","iconColorValue":"#111111","size":"has-small-icon-size","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"center"}} -->
+<ul class="wp-block-social-links has-small-icon-size has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"#","service":"twitter"} /-->
+
+<!-- wp:social-link {"url":"#","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"#","service":"instagram"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:post-author-biography /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
---
+++ patterns/post-author-profile-02.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Title: Post Author Profile 02
+ * Slug: hakoniwa/post-author-profile-02
+ * Categories: Hakoniwa
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|50","padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}}},"backgroundColor":"tertiary","layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+<div class="wp-block-group has-tertiary-background-color has-background" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:avatar {"size":48} /-->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:post-author-name {"textAlign":"left"} /-->
+
+<!-- wp:post-author-biography /-->
+
+<!-- wp:social-links {"iconColor":"contrast","iconColorValue":"#111111","size":"has-small-icon-size","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"left"}} -->
+<ul class="wp-block-social-links has-small-icon-size has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"#","service":"twitter"} /-->
+
+<!-- wp:social-link {"url":"#","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"#","service":"instagram"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
