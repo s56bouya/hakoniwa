bacd52a70b2d1dcf37cc0f11185b073d72abf12d
Tetsuya Imamura <s56bouya@gmail.com>
2023-08-19 10:30:17 +0900

    Update

---
+++ patterns/header-wide-and-search-button.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Title: Header wide and search button
+ * Slug: hakoniwa/header-wide-and-search-button
+ * Categories: header,Hakoniwa
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"layout":{"inherit":true,"type":"constrained"}} -->
+<div id="header" class="wp-block-group" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"style":{"spacing":{"blockGap":"1.5em"}},"layout":{"type":"flex"}} -->
+<div class="wp-block-group"><!-- wp:site-logo {"width":64} /-->
+
+<!-- wp:site-title /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:navigation {"ref":2893,"icon":"menu","layout":{"type":"flex","setCascadingProperties":true},"style":{"spacing":{"blockGap":"1.5em"}}} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:search {"label":"検索","showLabel":false,"buttonText":"検索","buttonPosition":"button-only","buttonUseIcon":true,"isSearchFieldHidden":true,"className":"header-modal"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
