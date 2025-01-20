a967efa561bdfe6dc6e5c3778f6de0ea77966fe0
Tetsuya Imamura <s56bouya@gmail.com>
2025-01-20 16:45:37 +0900

    Update
    ヘッダーパターンの調整
    is-hakoniwa〜 クラスの CSS プロパティの調整

--- patterns/header-catchphrase-02.php
+++ patterns/header-catchphrase-02.php
@@ -11,8 +11,8 @@
 
 ?>
 <!-- wp:group {"metadata":{"categories":["header","Hakoniwa"],"patternName":"hakoniwa/header","name":"Header"},"className":"hakoniwa-header-catchphrase-02","style":{"spacing":{"blockGap":"0"}},"layout":{"inherit":true,"type":"constrained"}} -->
-<div id="header" class="wp-block-group hakoniwa-header-catchphrase-02"><!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"padding":{"top":"0.25rem","bottom":"0.25rem"}}},"backgroundColor":"primary","textColor":"base","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull has-base-color has-primary-background-color has-text-color has-background has-link-color" style="padding-top:0.25rem;padding-bottom:0.25rem"><!-- wp:site-tagline {"align":"wide","fontSize":"2xs"} /--></div>
+<div id="header" class="wp-block-group hakoniwa-header-catchphrase-02"><!-- wp:group {"align":"full","className":"is-hakoniwa-smartphone-hidden","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"padding":{"top":"0.25rem","bottom":"0.25rem"}}},"backgroundColor":"primary","textColor":"base","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-base-color has-primary-background-color has-text-color has-background has-link-color is-hakoniwa-smartphone-hidden" style="padding-top:0.25rem;padding-bottom:0.25rem"><!-- wp:site-tagline {"align":"wide","fontSize":"2xs"} /--></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"align":"wide","className":"hakoniwa-header-main-content","layout":{"type":"flex","justifyContent":"space-between"}} -->
