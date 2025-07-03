4d25273399c68c5d4d183e6a129f61a347ed8d0f
Tetsuya Imamura <s56bouya@gmail.com>
2025-07-03 23:08:12 +0900

    Update
    モバイルファースト用のパターンとテンプレートを追加

---
+++ patterns/mobile-first.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: Mobile First
+ * Slug: hakoniwa/mobile-first
+ * Categories: Hakoniwa
+ * 
+ * @package Hakoniwa
+ * @since 1.0.0
+ */
+
+?>
+<!-- wp:cover {"dimRatio":10,"isUserOverlayColor":true,"minHeight":100,"minHeightUnit":"vh","isDark":false,"sizeSlug":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-cover is-light" style="margin-top:0;margin-bottom:0;min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-10 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"dimensions":{"minHeight":"100vh"},"spacing":{"padding":{"right":"0","left":"0"}}},"layout":{"type":"constrained","contentSize":"500px"}} -->
+<div class="wp-block-group" style="min-height:100vh;padding-right:0;padding-left:0"><!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}},"dimensions":{"minHeight":"100vh"},"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}},"shadow":"var:preset|shadow|hakoniwa-shadow-lg"},"backgroundColor":"base","textColor":"contrast","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-contrast-color has-base-background-color has-text-color has-background has-link-color" style="min-height:100vh;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;box-shadow:var(--wp--preset--shadow--hakoniwa-shadow-lg)"><!-- wp:post-content /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
